4f6b5a8243ecd69e88ac7a040aa81178c3a67393
IanWark <wark@ualberta.ca>
2019-02-07 20:41:05 -0700

    Fix input map: readd interact and fullscreen inputs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,6 +7,8 @@ const MOVE_SPEED = 4
 const MOUSE_SENS = 0.2
 const INTERACT_RANGE = 2 # Range player can interact with objects
 
+var INTERACT_PROMPT = "Press " + InputMap.get_action_list("interact")[0].as_text() + " to interact"
+
 onready var anim_player = $AnimationPlayer
 onready var raycast = $RayCast
 onready var prompt_label = $"CanvasLayer/Prompt Label"
@@ -30,7 +32,6 @@ func _input(event):
 		rotation_degrees.x -= MOUSE_SENS * event.relative.y
 		rotation_degrees.x = max(min(rotation_degrees.x, 85), -85)
 	if event.is_action_pressed("toggle_fullscreen"):
-		print("full screen")
 		OS.window_fullscreen = !OS.window_fullscreen
 
 func _process(delta):
@@ -55,32 +56,32 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("shoot") and !anim_player.is_playing():
 		
-		
 		if !get_node("Torch").visible:
 			anim_player.play("light")
 		else:
 			anim_player.play_backwards("light")
-			
+		
 		get_node("Torch").visible = !get_node("Torch").visible
 	
 	# Check if player is looking at anything interactable that is within range
 	var coll = raycast.get_collider()
 	if raycast.is_colliding() and coll != null and coll.has_method("interact"): 
 		if raycast.get_collision_point().distance_to(translation) < INTERACT_RANGE:
-			enable_prompt_label()
+			enable_interact_prompt()
 			
 			if Input.is_action_pressed("interact"):
 				coll.interact()
 		else:
-			disable_prompt_label()
+			disable_prompt()
 		
 	else:
-		disable_prompt_label()
+		disable_prompt()
 
-func enable_prompt_label():
+func enable_interact_prompt():
+	prompt_label.text = INTERACT_PROMPT
 	prompt_label.visible_characters = -1
 
-func disable_prompt_label():
+func disable_prompt():
 	prompt_label.visible_characters = 0
 
 func get_torch_visible():
