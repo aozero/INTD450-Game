ce6b4f58ecd92b0102a3e00c0d2ab80825169402
IanWark <wark@ualberta.ca>
2019-02-26 23:24:46 -0700

    Simplify forest wall trees to just sprites

--- Scripts/TreeForestWall.gd
+++ Scripts/TreeForestWall.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 func _ready():	
 	# Set sprite texture to a random tree in the sprite sheet
-	var sprite = $Sprite3D
+	var sprite = $"."
 	var rand_index = int(rand_range(0, sprite.hframes))
 	sprite.frame = rand_index
 	
