828f3b69cc3adba386768e3bf5e0331553a3462d
IanWark <wark@ualberta.ca>
2019-03-07 11:03:17 -0700

    Add more minor items with placeholder art

--- Scripts/Items/DialogueText.gd
+++ Scripts/Items/DialogueText.gd
@@ -1,9 +1,26 @@
 extends Node
 
+# Contains the text for all the dialogue in the game.
+# Make sure that every text constant starts and ends with quotation marks.
+# To make quotation marks in the actual text, use \" instead
+
+# You can make things bold or italicized or underlined etc. using BBCode, described here: 
+# https://docs.godotengine.org/en/3.0/tutorials/gui/bbcode_in_richtextlabel.html
+
 # Kitchen
-#const kitchen_
+const KITCHEN_JAR = "Oatmeal raisin! I thought they were chocolate chip? This is a jar of LIES!"
+const KITCHEN_MUG = "You don’t like your sandwich, Jamie? Come on, gluten free bread isn’t that bad."
 
-
+const KITCHEN_DRAWING = "This is a Boys Only Clubhouse, Jamie. If you want up, you gotta give me the password."
 
 # Study
 const STUDY_SHIP = "Daddy, you should read me [i]this[/i] story. It’s a pirate story."
+const STUDY_PLANNER = ""
+
+const STUDY_TAPSHOE = ""
+
+# Bedroom
+const BEDROOM_WATER = "[coughing] I wanted to swim without them. Davy and Jamie don’t have to wear them."
+const BEDROOM_TRICYCLE = "You can have a big kid bike when you’re tall enough to reach the pedals, silly girl. Why do you wanna grow up so fast anyway?"
+
+const BEDROOM_FLAMINGO = "" 
