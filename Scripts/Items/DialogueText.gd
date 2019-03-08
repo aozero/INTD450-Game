6208d90b6b2714aee95f1feeb59ef1c58ccaa27f
IanWark <wark@ualberta.ca>
2019-03-08 15:43:27 -0700

    Change DialogueText comments

--- Scripts/Items/DialogueText.gd
+++ Scripts/Items/DialogueText.gd
@@ -1,34 +1,40 @@
 extends Node
 
-# Contains the text for all the dialogue in the game.
-# For example, if you want to change text for the ship in a bottle,
-# change the content of what comes after STUDY_SHIP = "
-# Make sure that every text constant starts and ends with quotation marks.
-# To make quotation marks in the actual text, use \" instead
+"""
+ Contains the text for all the dialogue in the game.
+ Make sure that every text constant starts and ends with quotation marks.
+ To make quotation marks in the actual text, use \" instead
 
-# You can make things bold or italicized or underlined etc. using BBCode, described here: 
-# https://docs.godotengine.org/en/3.0/tutorials/gui/bbcode_in_richtextlabel.html
+ You can make things bold or italicized or underlined etc. using BBCode, described here: 
+ https://docs.godotengine.org/en/3.0/tutorials/gui/bbcode_in_richtextlabel.html
+"""
 
 # Kitchen
+###############################
 # Cookie Jar
 const KITCHEN_1 = "Oatmeal raisin! I thought they were chocolate chip? This is a jar of LIES!"
 # Coffee Mug
 const KITCHEN_2 = "You don’t like your sandwich, Jamie? Come on, gluten free bread isn’t that bad."
 # Drawing of Clubhouse
 const KITCHEN_FINAL = "This is a Boys Only Clubhouse, Jamie. If you want up, you gotta give me the password."
+###############################
 
 # Study
+###############################
 # Ship in a Bottle
 const STUDY_1 = "Daddy, you should read me [i]this[/i] story. It’s a pirate story."
 # Leather-bound Planner
 const STUDY_2 = ""
 # Tapshoe
 const STUDY_FINAL = ""
+###############################
 
 # Bedroom
+###############################
 # Water Wings
 const BEDROOM_1 = "[coughing] I wanted to swim without them. Davy and Jamie don’t have to wear them."
 # Tricycle
 const BEDROOM_2 = "You can have a big kid bike when you’re tall enough to reach the pedals, silly girl. Why do you wanna grow up so fast anyway?"
 # Flamingo
 const BEDROOM_FINAL = "" 
+###############################
