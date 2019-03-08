ad2695d893ff0d04b1b3408e5944d332763003a3
IanWark <wark@ualberta.ca>
2019-03-08 15:38:35 -0700

    Rename items to be numerical eg. StudyShip -> Study1

--- Scripts/Items/DialogueText.gd
+++ Scripts/Items/DialogueText.gd
@@ -10,19 +10,25 @@ extends Node
 # https://docs.godotengine.org/en/3.0/tutorials/gui/bbcode_in_richtextlabel.html
 
 # Kitchen
-const KITCHEN_JAR = "Oatmeal raisin! I thought they were chocolate chip? This is a jar of LIES!"
-const KITCHEN_MUG = "You don’t like your sandwich, Jamie? Come on, gluten free bread isn’t that bad."
-
-const KITCHEN_DRAWING = "This is a Boys Only Clubhouse, Jamie. If you want up, you gotta give me the password."
+# Cookie Jar
+const KITCHEN_1 = "Oatmeal raisin! I thought they were chocolate chip? This is a jar of LIES!"
+# Coffee Mug
+const KITCHEN_2 = "You don’t like your sandwich, Jamie? Come on, gluten free bread isn’t that bad."
+# Drawing of Clubhouse
+const KITCHEN_FINAL = "This is a Boys Only Clubhouse, Jamie. If you want up, you gotta give me the password."
 
 # Study
-const STUDY_SHIP = "Daddy, you should read me [i]this[/i] story. It’s a pirate story."
-const STUDY_PLANNER = ""
-
-const STUDY_TAPSHOE = ""
+# Ship in a Bottle
+const STUDY_1 = "Daddy, you should read me [i]this[/i] story. It’s a pirate story."
+# Leather-bound Planner
+const STUDY_2 = ""
+# Tapshoe
+const STUDY_FINAL = ""
 
 # Bedroom
-const BEDROOM_WATER = "[coughing] I wanted to swim without them. Davy and Jamie don’t have to wear them."
-const BEDROOM_TRICYCLE = "You can have a big kid bike when you’re tall enough to reach the pedals, silly girl. Why do you wanna grow up so fast anyway?"
-
-const BEDROOM_FLAMINGO = "" 
+# Water Wings
+const BEDROOM_1 = "[coughing] I wanted to swim without them. Davy and Jamie don’t have to wear them."
+# Tricycle
+const BEDROOM_2 = "You can have a big kid bike when you’re tall enough to reach the pedals, silly girl. Why do you wanna grow up so fast anyway?"
+# Flamingo
+const BEDROOM_FINAL = "" 
--- Scripts/Items/Minor/Bedroom1.gd
+++ Scripts/Items/Minor/Bedroom1.gd
@@ -1,4 +1,4 @@
 extends "res://Scripts/Items/Minor Item.gd"
 
 func _ready():
-	memory_text = get_node("/root/DialogueText").STUDY_SHIP
+	memory_text = get_node("/root/DialogueText").BEDROOM_2
