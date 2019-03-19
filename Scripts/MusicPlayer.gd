bde9ba23d83986d782c3dbc27ce3439c2e7d814a
IanWark <wark@ualberta.ca>
2019-03-18 20:45:21 -0600

    Fade in drone

--- Scripts/MusicPlayer.gd
+++ Scripts/MusicPlayer.gd
@@ -2,16 +2,21 @@ extends Node
 
 const FADE_DURATION = 2.0
 
+onready var tween_in = $TweenIn
 onready var tween_out = $TweenOut
 
 onready var drone_player = $DronePlayer
 onready var drone_vol = drone_player.volume_db
-
 onready var melody_player = $MelodyPlayer
 onready var melody_vol = melody_player.volume_db
-
 onready var dying_player = $DyingPlayer
 onready var dying_vol = dying_player.volume_db
+
+func _ready():
+	drone_player.volume_db = -80
+	drone_player.play()
+	tween_in.interpolate_property(drone_player, "volume_db", -80, drone_vol, FADE_DURATION, Tween.TRANS_SINE, Tween.EASE_IN, 0)
+	tween_in.start()
 
 func play_melody(melody):
 	melody_player.stream = melody
