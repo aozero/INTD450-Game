1a9d1fd70d0d4851be9c3c28f9ab2098c7368430
IanWark <wark@ualberta.ca>
2019-03-18 20:43:08 -0600

    Fade out memory music

--- Scripts/MusicPlayer.gd
+++ Scripts/MusicPlayer.gd
@@ -1,15 +1,28 @@
 extends Node
 
+const FADE_DURATION = 2.0
+
+onready var tween_out = $TweenOut
+
 onready var drone_player = $DronePlayer
+onready var drone_vol = drone_player.volume_db
+
 onready var melody_player = $MelodyPlayer
+onready var melody_vol = melody_player.volume_db
+
 onready var dying_player = $DyingPlayer
+onready var dying_vol = dying_player.volume_db
 
 func play_melody(melody):
 	melody_player.stream = melody
 	melody_player.play()
 
 func stop_melody():
-	melody_player.stop()
+	tween_out.interpolate_property(melody_player, "volume_db", melody_vol, -80, FADE_DURATION, Tween.TRANS_SINE, Tween.EASE_IN, 0)
+	tween_out.start()
 
 func play_dying():
 	dying_player.play()
+
+func _on_TweenOut_tween_completed(object, key):
+	object.stop()
