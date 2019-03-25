e632238e8d9101c58fc7ff6b69240e33eebd39f1
IanWark <wark@ualberta.ca>
2019-03-24 20:20:34 -0600

    Add basic main menu

---
+++ Scripts/Menus/MainMenu.gd
@@ -0,0 +1,13 @@
+extends ColorRect
+
+onready var options_menu = $OptionsMenu
+onready var exit_verification = $ExitVerfication
+
+func _on_StartButton_button_up():
+	get_tree().change_scene("res://Scenes/Intro.tscn")
+
+func _on_OptionsButton_button_up():
+	options_menu.visible = true
+
+func _on_ExitButton_button_up():
+	exit_verification.visible = true
