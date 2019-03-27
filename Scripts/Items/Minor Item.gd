1f9548dc719dbd591cdf4a474438fb0fe2540282
IanWark <wark@ualberta.ca>
2019-03-26 19:47:34 -0600

    Only play minor item audio if no other memory is playing

--- Scripts/Items/Minor Item.gd
+++ Scripts/Items/Minor Item.gd
@@ -3,4 +3,5 @@ extends "res://Scripts/Items/Interactable.gd"
 var DIALOGUE # Container with TEXT, SOUND, and TEXT_TIME. Set by inherting classes
 
 func interact(player):
-	player.play_dialogue(DIALOGUE)
+	if !player.memory_controller.dialogue_audio.is_playing():
+		player.play_dialogue(DIALOGUE)
