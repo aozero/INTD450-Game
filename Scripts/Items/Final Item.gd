27348314c261f1ac90e4fd3b6256594d353ac485
IanWark <wark@ualberta.ca>
2019-03-26 19:30:17 -0600

    Start adding burning animation to final items

--- Scripts/Items/Final Item.gd
+++ Scripts/Items/Final Item.gd
@@ -1,11 +1,23 @@
 extends "res://Scripts/Items/Interactable.gd"
 
+export var burning = false
+
  # Set by child scripts
 var DIALOGUE # Container with TEXT, SOUND, and MUSIC. Set by inherting classes
 
 onready var sprite = $CollisionShape/Sprite3D
+onready var interactable_particles = $Particles
+onready var burn_anim = $BurnAnim
 
 var interacted_with = false
+
+func _ready():
+	if burning:
+		interacted_with = true
+		interactable_particles.visible = false
+		sprite.material_override = load("res://Materials/FireBillboard.tres").duplicate()
+		translation.y = 0.3
+		burn_anim.play("burn")
 
 func interact(player):
 	if !interacted_with:
