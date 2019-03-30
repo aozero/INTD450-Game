6b04d673d16be9453dcea3cee4c483662032c5fe
IanWark <wark@ualberta.ca>
2019-03-30 13:30:37 -0600

    Light ground differently than objects to reduce floaty trees
    Grounds is lit less intensely but at longer distances than before

--- Scripts/Fires/FireLight.gd
+++ Scripts/Fires/FireLight.gd
@@ -1,10 +1,16 @@
 extends OmniLight
 
+onready var ground_light = $GroundLight
 onready var globals = get_node("/root/Globals")
 
 func _ready():
 	add_to_group("lights")
 	set_shadows(globals.get_shadows())
+	
+	# Light the ground differently - less powerfully, but longer distance
+	ground_light.omni_range = omni_range
+	ground_light.light_color = light_color
+	ground_light.light_energy = light_energy / 3.0
 
 func set_shadows(value):
 	shadow_enabled = value
