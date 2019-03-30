2368016fb78d81675d8a88cfc3ca976c41611075
IanWark <wark@ualberta.ca>
2019-03-30 00:09:37 -0600

    Bedroom: Add burning animation to tapshoe

--- Scripts/Items/Final/StudyFinal.gd
+++ Scripts/Items/Final/StudyFinal.gd
@@ -2,6 +2,8 @@ extends "res://Scripts/Items/Final Item.gd"
 
 func _ready():
 	DIALOGUE = get_node("/root/Dialogue").STUDY_FINAL
+	BURN_ANIM_NAME = "burn"
+	._ready()
 
 func after_memory():
 	get_tree().change_scene("res://Scenes/Worlds/Bedroom.tscn")
