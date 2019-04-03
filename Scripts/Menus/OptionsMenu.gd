2b661b7a0c84f059d52f6fe01c0ce639a0c4eac3
IanWark <wark@ualberta.ca>
2019-04-02 22:47:01 -0600

    Make volume sliders work

--- Scripts/Menus/OptionsMenu.gd
+++ Scripts/Menus/OptionsMenu.gd
@@ -1,9 +1,17 @@
 extends ColorRect
 
+# DISPLAY
 onready var fullscreen_box = $VBoxContainer/Tabs/Display/Right/Fullscreen/FullscreenBox
 onready var brightness_slider = $VBoxContainer/Tabs/Display/Right/Brightness/BrightnessSlider
 onready var quality_button = $VBoxContainer/Tabs/Display/Right/ForestQuality/QualityButton
 onready var shadow_box = $VBoxContainer/Tabs/Display/Right/Shadows/ShadowBox
+
+# AUDIO
+onready var master_slider = $VBoxContainer/Tabs/Audio/Right/Master/MasterSlider
+onready var music_slider = $VBoxContainer/Tabs/Audio/Right/Music/MusicSlider
+onready var effects_slider = $VBoxContainer/Tabs/Audio/Right/Effects/EffectsSlider
+onready var voice_slider = $VBoxContainer/Tabs/Audio/Right/Voice/VoiceSlider
+
 onready var back_button = $VBoxContainer/BackContainer/BackButton
 onready var click_audio = $ClickAudio
 onready var globals = get_node("/root/Globals")
@@ -18,6 +26,11 @@ func _on_OptionsMenu_visibility_changed():
 	brightness_slider.value = globals.get_brightness()
 	quality_button.select(globals.get_forest_quality())
 	shadow_box.pressed = globals.get_shadows()
+	
+	master_slider.value = get_volume("Master")
+	music_slider.value = get_volume("Music")
+	effects_slider.value = get_volume("Effects")
+	voice_slider.value = get_volume("Voice")
 
 func _on_BackButton_button_up():
 	click_audio.play()
@@ -47,3 +60,23 @@ func _on_ShadowBox_button_up():
 	click_audio.play()
 	# is_pressed returns true when the check mark is not there
 	globals.set_shadows(!shadow_box.is_pressed())
+
+# Bus is a string with the name of the bus
+func get_volume(bus):
+	return AudioServer.get_bus_volume_db(AudioServer.get_bus_index(bus))
+
+# Bus is a string with the name of the bus
+func set_volume(bus, value):
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), value)
+
+func _on_MasterSlider_value_changed(value):
+	set_volume("Master", value)
+
+func _on_MusicSlider_value_changed(value):
+	set_volume("Music", value)
+
+func _on_EffectsSlider_value_changed(value):
+	set_volume("Effects", value)
+
+func _on_VoiceSlider_value_changed(value):
+	set_volume("Voice", value)
