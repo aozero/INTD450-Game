bd745f4b58356e63c337113b91f511ddd7d4a3db
IanWark <wark@ualberta.ca>
2019-04-06 01:49:41 -0600

    Allow logo to be skipped with ESC

--- Scripts/Menus/Logo.gd
+++ Scripts/Menus/Logo.gd
@@ -6,4 +6,11 @@ func _ready():
 	anim_player.play("fade")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
+	next_scene()
+
+func _input(event):
+	if event.is_action_pressed("pause"):
+		next_scene()
+
+func next_scene():
 	get_tree().change_scene("res://Scenes/Menus/DarkAdvisory.tscn")
