c5d99c60ff2ba699d03a55061c9be15607f96264
IanWark <wark@ualberta.ca>
2019-04-06 04:01:07 -0600

    Multiple small fixes to Dialogue.gd

--- Scripts/Items/Dialogue.gd
+++ Scripts/Items/Dialogue.gd
@@ -89,38 +89,37 @@ func _ready():
 	TUTORIAL_1.TEXT_TIME = 16
 	
 	# Right after moving past the campfire
-	var run_button = get_action_button_text("run")
 	TUTORIAL_2.SOUND = load("res://Sound/Effects/Memory/Minor Items/silence.wav")
 	TUTORIAL_2["None"] = "Running (Hold Shift) in the forest makes a lot of noise. "
 	TUTORIAL_2["Español"] = "Correr en el bosque (Manteniendo la tecla Shift) hace mucho ruido."
 	TUTORIAL_2.TEXT_TIME = 16
 	
 	# At the very edge of the campfire's light
-	var light_button = get_action_button_text("shoot")
 	TUTORIAL_3.SOUND = load("res://Sound/Effects/Memory/Minor Items/silence.wav")
 	TUTORIAL_3["None"] = "Lighting a match (Left Click) will make it easier to see... and be seen."
 	TUTORIAL_3["Español"] = "Encender un fósforo (tecla izquierda del ratón) hará que sea más fácil de ver… y ser visto."
+	TUTORIAL_3.TEXT_TIME = 16
 	###############################
 	
 	# Kitchen
 	###############################
 	# Cookie Jar
 	KITCHEN_1.SOUND = load("res://Sound/Effects/Memory/Minor Items/kitchen_cookiejar_memory.wav")
-	KITCHEN_1.None = ""
-	KITCHEN_1["English"] = "Riley: Oatmeal raisin! I thought they were chocolate chip? This is jar is full of LIES!"
+	KITCHEN_1["None"] = ""
+	KITCHEN_1["English"] = "Riley: Oatmeal raisin! I thought they were chocolate chip? This jar is full of LIES!"
 	KITCHEN_1["Español"] = "Riley: ¿Avena y pasas? ¡Pensé que eran pepitas de chocolate! ¡Este es un tarro de mentiras!"
 	KITCHEN_1.TEXT_TIME = 8
 	
 	# Coffee Mug
 	KITCHEN_2.SOUND = load("res://Sound/Effects/Memory/Minor Items/kitchen_coffeemug_memory.wav")
-	KITCHEN_2.None = ""
+	KITCHEN_2["None"] = ""
 	KITCHEN_2["English"] = "Mom: Oh NOW you don’t like your sandwich? Come on, you would have never known it was vegan cheese if I hadn't told you. The store said it tasted exactly the same."
 	KITCHEN_2["Español"] = "Mamá: Ah, ¿ahora no quieres tu sándwich? Nunca lo hubieras sabido fui queso vegano si no te hubiera dicho. La cajera dijo que el sabor fue el mismo."
 	KITCHEN_2.TEXT_TIME = 10
 	
 	# Drawing of Clubhouse
 	KITCHEN_FINAL.SOUND = load("res://Sound/Effects/Memory/Final Items/kitchen_memory.wav")
-	KITCHEN_FINAL.None = ""
+	KITCHEN_FINAL["None"] = ""
 	KITCHEN_FINAL["English"] = "Riley: Too slow, Jamie. If you want me to drop the ladder, you gotta give me the password."
 	KITCHEN_FINAL["Español"] = "Riley: Demasiado lento, Jaime. Si tú me quieres soltar la escalera, tienes que dame la contraseña."
 	KITCHEN_FINAL.MUSIC = load("res://Sound/Music/KitchenMemory.wav")
@@ -130,20 +129,20 @@ func _ready():
 	###############################
 	# Ship in a Bottle
 	STUDY_1.SOUND = load("res://Sound/Effects/Memory/Minor Items/study_shipinabottle_memory.wav")
-	STUDY_1.None = ""
+	STUDY_1["None"] = ""
 	STUDY_1["English"] = "Riley: I'm sorry, Daddy! It was an accident, I swear. I didn't think the ball would go that far."
 	STUDY_1["Español"] = "Riley: ¡Lo siento, Papá! ¡Fue un accidente, prometo! No pensé que la pelota llegaría tan lejos."
 	STUDY_1.TEXT_TIME = 8
 	
 	# Leather-bound Planner
 	STUDY_2.SOUND = load("res://Sound/Effects/Memory/Minor Items/silence.wav")
-	STUDY_2.None = "Dad: May 14th, 2009. Jamie (Caterpillar) and Riley's (Queen of Hearts) school play. Alice in Wonderland - school gym 4:30PM. [crossed out]. Red eye flight to Taiwan 5:40 AM."
+	STUDY_2["None"] = "Dad: May 14th, 2009. Jamie (Caterpillar) and Riley's (Queen of Hearts) school play. Alice in Wonderland - school gym 4:30PM. [crossed out]. Red eye flight to Taiwan 5:40 AM."
 	STUDY_2["Español"] = "Papá: 14 de mayo, 2009. Jaime (Oruga) y Riley (La Reina de Corazones) obra escolar. Alicia en el país de las Maravillas – gimnasio de escuela, 16:30. Vuelo nocturno a Taiwán, 5:40."
 	STUDY_2.TEXT_TIME = 12
 	
 	# Tapshoe
 	STUDY_FINAL.SOUND = load("res://Sound/Effects/Memory/Final Items/study_memory.wav")
-	STUDY_FINAL.None = ""
+	STUDY_FINAL["None"] = ""
 	STUDY_FINAL["English"] = "Dad: Jamie, get your sister to cool it would ya? I'm getting a headache."
 	STUDY_FINAL["Español"] = "Papá: Jaime, haz que tu hermana lo detenga por favor. Me duele la cabeza."
 	STUDY_FINAL.MUSIC = load("res://Sound/Music/StudyMemory.wav")
@@ -153,21 +152,21 @@ func _ready():
 	###############################
 	# Ice Skate
 	BEDROOM_1.SOUND = load("res://Sound/Effects/Memory/Minor Items/bedroom_iceskate_memory.wav")
-	BEDROOM_1.None = ""
+	BEDROOM_1["None"] = ""
 	BEDROOM_1["English"] = "Riley: Jaime, watch me! Look at me. I'm a dancer."
 	BEDROOM_1["Español"] = "Riley: Jaime ¡mírame! ¡Mírame, Jaime! Soy una bailarina."
 	BEDROOM_1.TEXT_TIME = 8
 	
 	# Small Painting Easel
 	BEDROOM_2.SOUND = load("res://Sound/Effects/Memory/Minor Items/bedroom_easel_memory.wav")
-	BEDROOM_2.None = ""
+	BEDROOM_2["None"] = ""
 	BEDROOM_2["English"] = "Mom: Sweetie, I love all of your art but if you don't clean this room, I'm taking all your paint and crayons away for a week."
 	BEDROOM_2["Español"] = "Mamá: Cariña, me encanta todo tu arte, pero si no limpias esta habitación, te quitaré toda la pintura y los crayones durante una semana."
 	BEDROOM_2.TEXT_TIME = 9
 	
 	# Flamingo
 	BEDROOM_FINAL.SOUND = load("res://Sound/Effects/Memory/Final Items/bedroom_memory.wav")
-	BEDROOM_FINAL.None = ""
+	BEDROOM_FINAL["None"] = ""
 	BEDROOM_FINAL["English"] = "Riley: Jamie? I can't see you. Ouch! Oh my gosh, it's burning! Jamie, where are you? H-help!" 
 	BEDROOM_FINAL["Español"] = "Riley: ¡Jaime! No puedo verte. ¡Ay!¡Dios mio! Está quemando. Jaime, ¿dondé estás? ¡Ayudame!"
 	BEDROOM_FINAL.MUSIC = load("res://Sound/Music/BedroomMemory.wav")
@@ -179,6 +178,7 @@ func get_subtitles(item_dialogue):
 	else:
 		return item_dialogue[SUBTITLES_SUPPORTED[0]]
 
+# No longer used as keys cannot be found and it is simpler for translation to hard code
 func get_action_button_text(action):
 	var first = InputMap.get_action_list(action)[0]
 	if first.is_class("InputEventMouseButton"):
