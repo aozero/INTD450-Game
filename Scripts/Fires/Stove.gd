caf65f9e172d55487bc70b377ab4b20f7f988856
IanWark <wark@ualberta.ca>
2019-04-12 14:37:07 -0600

    Add animated stove

--- Scripts/Fires/Stove.gd
+++ Scripts/Fires/Stove.gd
@@ -2,6 +2,7 @@ extends StaticBody
 
 onready var sprite = $CollisionShape/Spatial/Sprite3D
 onready var light = $CollisionShape/Spatial/Sprite3D/FireLight
+onready var stove_anim = $StoveAnim
 
 export var on = false
 
@@ -9,10 +10,10 @@ func _ready():
 	light.visible = on
 	
 	if on:
-		sprite.frame = 1
+		stove_anim.play("lit")
 		# If on, use a different layer so the match doesn't add to the brightness of the fire
 		sprite.layers = 524288
 	else:
-		sprite.frame = 0
+		stove_anim.play("unlit")
 		# If off, use normal layer
 		sprite.layers = 1
